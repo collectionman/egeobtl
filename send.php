6209703090aa8c7ef75c3cac67839ff236e23a76
collectionman <julianllorensi@outlook.com>
2019-01-11 17:00:48 -0300

    send file

---
+++ send.php
@@ -0,0 +1,25 @@
+<!--
+    To send a mail to egepbtl@gmail.com
+-->
+
+<!-- form variables -->
+$name = $_POST[ 'name' ] ;
+$email = $_POST[ 'email' ] ;
+$message = $_POST[ 'message' ] ;
+
+<!-- email address -->
+$to = 'egeobtl@gmail.com' ;
+$subject = 'EGEO' ;
+$header = "Form: $email" ;
+$messageCompleted = "Nombre: $name\n E-mail: $email Mensaje:\n $message" ; 
+
+if ( $_POST[ 'submit' ] ) {
+    if ( mail( $to, $subject, $messageCompleted, $header ) ) {
+        echo "<script language='javascript'>
+            alert( 'Tu mensaje ha sido enviado correctamente, pronto te responderemos' ) ;
+        </script>" ;
+    } else {
+        echo 'fallo al enviar correo' ;
+    }
+}
+
